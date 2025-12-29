8e448fbfdcf7afe3c237873f3578dfe6fae7a196
AtharvNatu <natuatharv21@gmail.com>
2025-12-29 14:55:39 +0530

    Vulkan-Linux: Updated Directory Structure and Git Tracking

--- 01-Vulkan/02-Linux/09-Texture/03-Pyramid/Build.sh
+++ 01-Vulkan/02-Linux/09-Texture/03-Pyramid/Build.sh
@@ -2,26 +2,42 @@
 
 clear
 
+API="Vulkan"
+
 VULKAN_INCLUDE_PATH="/home/atharv/VulkanSDK/Vulkan/x86_64/include"
 VULKAN_LIB_PATH="/home/atharv/VulkanSDK/Vulkan/x86_64/lib"
 VULKAN_BIN_PATH="/home/atharv/VulkanSDK/Vulkan/x86_64/bin"
 
 GLM_INCLUDE_PATH="/home/atharv/Desktop/Workspace/ARTR/01-Vulkan/02-Linux/External/glm"
 
-API="Vulkan"
+SOURCE_PATH=Source
+INCLUDE_PATH=Include
+IMAGES_PATH=Assets\Images
+
+BIN_DIR=Bin
 SPV=1
 
-rm -f *.o 
+mkdir -p "$BIN_DIR"
+
+clear
+
+rm -f "$BIN_DIR"/*.o 2>/dev/null
 
 echo "--------------------------------------------------------------------------------"
 echo "Compiling X11 and $API Source Code ..."
 echo "--------------------------------------------------------------------------------"
-g++ -c -o Vk.o -I $VULKAN_INCLUDE_PATH -I $GLM_INCLUDE_PATH Vk.cpp
+g++ -c \
+    -o Vk.o \
+    -I"$VULKAN_INCLUDE_PATH" \
+    -I"$GLM_INCLUDE_PATH" \
+    "$SOURCE_PATH/Vk.cpp"
 
 if [ $? -ne 0 ]; then
     echo "Compilation Failed !!!"
     exit 1
 fi
+
+mv *.o "$BIN_DIR" 2>/dev/null
 
 if (( SPV == 1)); then
     echo
@@ -35,8 +51,8 @@ if (( SPV == 1)); then
             echo "Shader Compilation Failed !!!"
             exit 1
         fi
-        mv Bin/Shader.vert.spv ../
-        mv Bin/Shader.frag.spv ../
+        mv Shader.vert.spv ../"$BIN_DIR"
+        mv Shader.frag.spv ../"$BIN_DIR"
     cd ..
 fi
 
@@ -45,7 +61,11 @@ echo "--------------------------------------------------------------------------
 echo "Linking Object Files and Creating Binary ..."
 echo "--------------------------------------------------------------------------------"
 
-g++ Vk.o -o Vk -lX11 -lm -L$VULKAN_LIB_PATH -lvulkan
+g++ "$BIN_DIR"/Vk.o -o Vk \
+    -lX11 \
+    -lm \
+    -L"$VULKAN_LIB_PATH" \
+    -lvulkan
 
 if [ $? -ne 0 ]; then
     echo "Linking Failed !!!"
