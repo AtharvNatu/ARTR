11ea80b0deace75cce1ed7a4544c29a644c3635f
AtharvNatu <natuatharv21@gmail.com>
2026-07-23 01:47:29 +0530

    WebGPU: Added Perspective White Triangle

--- 02-WebGPU/02-Perspective-Triangle/Source/Canvas.js
+++ 02-WebGPU/02-Perspective-Triangle/Source/Canvas.js
@@ -11,6 +11,11 @@ let context = null;
 let queue = null;
 let canvasFormat = null;
 let animationFrameId = null;
+let buffer_position = null;
+let render_pipeline = null;
+let buffer_mvpUniform = null;
+let bindGroup_mvpUniform = null;
+let perspectiveProjectionMatrix = null;
 
 //* Animation Related
 var requestAnimationFrame = window.requestAnimationFrame ||                // Chrome
@@ -98,7 +103,7 @@ async function main()
     device.lost.then(onDeviceLost);
 
     // Initialize
-    initialize();
+    await initialize();
 
     // Warmup Resize
     resize();
@@ -118,7 +123,14 @@ function onDeviceLost(info)
 {
     // Code
     console.warn("WebGPU Device Lost : ", info.reason, ", Message : ", info.message);
+
     queue = null;
+
+    buffer_position = null;
+    render_pipeline = null;
+    buffer_mvpUniform = null;
+    bindGroup_mvpUniform = null;
+    perspectiveProjectionMatrix = null;
 }
 
 function toggleFullScreen()
@@ -171,7 +183,7 @@ function onFullScreenChange()
     resize();
 }
 
-function initialize()
+async function initialize()
 {
     // Code
 
@@ -211,6 +223,264 @@ function initialize()
         b: 1.0,
         a: 1.0
     };
+
+    //* Load Shaders From File
+    const triangleVertWGSL = await loadShader("../Shaders/triangle.vert.wgsl");
+    const triangleFragWGSL = await loadShader("../Shaders/triangle.frag.wgsl");
+
+    //* Vertex Shader Module
+    const shaderModuleDescriptor_vertexShader = 
+    {
+        code: triangleVertWGSL
+    };
+
+    const shaderModule_vertexShader = device.createShaderModule(shaderModuleDescriptor_vertexShader);
+    if (shaderModule_vertexShader == null)
+    {
+        console.log("Failed To Create Vertex Shader Module !!!");
+        throw Error("Failed To Create Vertex Shader Module !!!");
+    }
+    else
+        console.log("Vertex Shader Module Successfully Created");
+
+    //* Fragment Shader Module
+    const shaderModuleDescriptor_fragmentShader = 
+    {
+        code: triangleFragWGSL
+    };
+
+    const shaderModule_fragmentShader = device.createShaderModule(shaderModuleDescriptor_fragmentShader);
+    if (shaderModule_fragmentShader == null)
+    {
+        console.log("Failed To Create Fragment Shader Module !!!");
+        throw Error("Failed To Create Fragment Shader Module !!!");
+    }
+    else
+        console.log("Fragment Shader Module Successfully Created");
+
+    const vertex_position = new Float32Array([
+        0.0,    1.0,    0.0,    1.0,
+        -1.0,   -1.0,   0.0,    1.0,
+        1.0,    -1.0,   0.0,    1.0
+    ]);
+
+    //* Vertex Buffer For Position
+    
+    const bufferDescriptor_position = 
+    {
+        size: vertex_position.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+    };
+
+    buffer_position = device.createBuffer(bufferDescriptor_position);
+    if (buffer_position == null)
+    {
+        console.log("Failed To Create Vertex Buffer For Position !!!");
+        throw Error("Failed To Create Vertex Buffer For Position !!!");
+    }
+    else
+        console.log("Vertex Buffer For Position Successfully Created");
+
+    queue.writeBuffer(buffer_position, 0, vertex_position, 0, vertex_position.length);
+    console.log("Position Data Written To Vertex Buffer");
+
+    //! Uniform Plumbing
+
+    //* Step - 1: Bind Group Layout for MVP Uniform
+
+    //* Step - 1A: Bind Group Layout Entry
+    const bindGroupLayoutEntry_mvpUniform = 
+    {
+        binding: 0, // Maps to 0th group's 0th slot binding in Vertex Shader
+        visibility: GPUShaderStage.VERTEX,
+        buffer: 
+        {
+            type: "uniform"
+        }
+    };
+
+    //* Step - 1B: Bind Group Layout Descriptor
+    const bindGroupLayoutDescriptor = 
+    {
+        entries: 
+        [
+            bindGroupLayoutEntry_mvpUniform
+        ]
+    };
+
+    //* Step - 1C: Bind Group Layout
+    const bindGroupLayout_mvpUniform = device.createBindGroupLayout(bindGroupLayoutDescriptor);
+    if (bindGroupLayout_mvpUniform == null)
+    {
+        console.log("Failed To Create Bind Group Layout For MVP Uniform !!!");
+        throw Error("Failed To Create Bind Group Layout For MVP Uniform !!!");
+    }
+    else
+        console.log("Bind Group Layout For MVP Uniform Successfully Created");
+
+    //* Step - 2: Pipeline Layout for MVP Uniform
+
+    //* Step - 2A: Pipeline Layout Descriptor
+    const pipelineLayoutDescriptor = 
+    {
+        bindGroupLayouts:
+        [
+            bindGroupLayout_mvpUniform
+        ]
+    };
+
+    //* Step - 2B: Pipeline Layout
+    const pipelineLayout = device.createPipelineLayout(pipelineLayoutDescriptor);
+    if (pipelineLayout == null)
+    {
+        console.log("Failed To Create Pipeline Layout For MVP Uniform !!!");
+        throw Error("Failed To Create Pipeline Layout For MVP Uniform !!!");
+    }
+    else
+        console.log("Pipeline Layout For MVP Uniform Successfully Created");
+
+    //* Step - 3: Uniform Buffer for MVP Uniform
+
+    //* Step - 3A: Buffer Descriptor
+    const mvpUniformSize = 4 * 16;
+
+    const bufferDescriptor_mvpUniform = 
+    {
+        size: mvpUniformSize,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+    };
+
+    buffer_mvpUniform = device.createBuffer(bufferDescriptor_mvpUniform);
+    if (buffer_mvpUniform == null)
+    {
+        console.log("Failed To Create Uniform Buffer For MVPUniform !!!");
+        throw Error("Failed To Create Uniform Buffer For MVPUniform !!!");
+    }
+    else
+        console.log("Uniform Buffer For MVPUniform Successfully Created");
+
+    //* Step - 4: Bind Group For Uniform Buffer
+
+    //* Step - 4A: Buffer Binding Property
+    const bufferBinding_mvpUniform = 
+    {
+        buffer: buffer_mvpUniform,
+        offset: 0,
+        size: mvpUniformSize
+    };
+
+    //* Step - 4B: Buffer Binding Entry
+    const bindGroupEntry_mvpUniform = 
+    {
+        binding: 0,
+        resource: bufferBinding_mvpUniform
+    };
+
+    //* Step - 4C: Buffer Binding Descriptor
+    const bindGroupDescriptor_mvpUniform = 
+    {
+        layout: bindGroupLayout_mvpUniform,
+        entries:
+        [
+            bindGroupEntry_mvpUniform
+        ]
+    };
+
+    //* Step - 4D: Bind Group
+    bindGroup_mvpUniform = device.createBindGroup(bindGroupDescriptor_mvpUniform);
+    if (bindGroup_mvpUniform == null)
+    {
+        console.log("Failed To Create Bind Group For MVP Uniform !!!");
+        throw Error("Failed To Create Bind Group For MVP Uniform !!!");
+    }
+    else
+        console.log("Bind Group For MVP Uniform Successfully Created");
+
+
+    //! Render Pipeline
+
+    //* Step - 1 : Pipeline Descriptor / PSO
+
+    //* Step - 1A: Vertex Buffer Layout
+    const positionVertexAttribute = 
+    {
+        shaderLocation: 0,  //* Maps to location(0) in Vertex Shader
+        offset: 0,
+        format: "float32x4"
+    };
+
+    const positionVertexBufferLayout = 
+    {
+        attributes: 
+        [
+            positionVertexAttribute
+        ],
+        arrayStride: 4 * 4,
+        stepMode: "vertex"  // Jump vertex by vertex, not instance by instance
+    };
+
+    //* Step - 1B: Vertex Shader State
+    const vertexShaderState = 
+    {
+        module: shaderModule_vertexShader,
+        entryPoint: "main",
+        buffers: 
+        [
+            positionVertexBufferLayout
+        ]
+    };
+
+    //* Step - 1C: Fragment Shader State
+    const colorTargetState = 
+    {
+        format: canvasFormat
+    };
+
+    const fragmentShaderState = 
+    {
+        module: shaderModule_fragmentShader,
+        entryPoint: "main",
+        targets:
+        [
+            colorTargetState
+        ]
+    };
+
+    //* Step - 1D: Primitive State
+    const primitiveState = 
+    {
+        frontFace: "ccw",    // Counter-Clockwise
+        cullMode: "none",    // No Culling
+        topology: "triangle-list"
+    };
+
+    //* Step - 1E: PSO
+    const pipelineDescriptor = 
+    {
+        layout: pipelineLayout,
+        vertex: vertexShaderState,
+        fragment: fragmentShaderState,
+        primitive: primitiveState
+    };
+
+    render_pipeline = device.createRenderPipeline(pipelineDescriptor);
+    if (render_pipeline == null)
+    {
+        console.log("Failed To Create Render Pipeline !!!");
+        throw Error("Failed To Create Render Pipeline !!!");
+    }
+    else
+        console.log("Render Pipeline Successfully Created");
+
+    //* Initialize Projection Matrix
+    perspectiveProjectionMatrix = mat4.create();
+}
+
+async function loadShader(path)
+{
+    // Code
+    const response = await fetch(path);
+    return await response.text();
 }
 
 function resize()
@@ -226,6 +496,8 @@ function resize()
         canvas.width = canvas_original_width;
         canvas.height = canvas_original_height;
     }
+
+    mat4.perspective(perspectiveProjectionMatrix, 45.0, parseFloat(canvas.width) / parseFloat(canvas.height), 0.1, 100.0);
 }
 
 function display()
@@ -256,9 +528,27 @@ function display()
     {
         colorAttachments: [renderPassColorAttachment]
     };
+    
+    //! Transformations
+    var modelViewMatrix = mat4.create();
+    var modelViewProjectionMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -3.0]);
+    mat4.multiply(modelViewProjectionMatrix, perspectiveProjectionMatrix, modelViewMatrix);
+
+    //! Update Uniform Buffer
+    queue.writeBuffer(buffer_mvpUniform, 0, modelViewProjectionMatrix, 0, modelViewProjectionMatrix.length);
 
     //* Step - 13 : Begin The Render Pass
     const renderPassEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    {
+        renderPassEncoder.setPipeline(render_pipeline);
+        renderPassEncoder.setViewport(0, 0, canvas.width, canvas.height, 0.0, 1.0);
+        renderPassEncoder.setScissorRect(0, 0, canvas.width, canvas.height);
+        renderPassEncoder.setVertexBuffer(0, buffer_position);
+        renderPassEncoder.setBindGroup(0, bindGroup_mvpUniform);
+        renderPassEncoder.draw(3);
+    }
     renderPassEncoder.end();
 
     //* Step - 14 : Finish The Command Encoder And Submit To Queue
@@ -295,7 +585,13 @@ function uninitialize()
         device.destroy();
         device = null;
         queue = null;
+        buffer_position = null;
+        render_pipeline = null;
+        buffer_mvpUniform = null;
+        bindGroup_mvpUniform = null;
     }
+
+    perspectiveProjectionMatrix = null;
 }
 
 function keyDown(event)
